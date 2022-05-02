0280befedfcd2d97e655e99caf72932e2b6305e4
mitlIE <86919933+mitlIE@users.noreply.github.com>
2022-05-02 22:18:32 +0100

    Fixed form reset and styled notifications

--- main.js
+++ main.js
@@ -1,23 +1,25 @@
 /* Get the required form elements */
-var myField = document.querySelector('.emailAddr').value;
+var myForm = document.querySelector('.subForm');
+var myField = document.querySelector('.emailAddr');
 var myBtn = document.querySelector('.subBtn');
 var myMsg = document.querySelector('.msg');
-var emailRegex = /^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/i;
+var emailRegex = /^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/;
 
 // When the Submit button is clicked pass in the event
 myBtn.addEventListener('click', function(event){
     event.preventDefault();
 
     // Test to see if field is empty or has content
-    if(myField !== '' && emailRegex.test(myField)) {
+    if(myField.value != "" && emailRegex.test(myField.value))  {
 
         myMsg.classList.add('msg-show');
         myMsg.innerHTML = "You have successfully subscribed!";
+        myForm.reset();
     }
     else
     {
         myMsg.classList.add('msg-show');
         myMsg.innerHTML = "Please enter a valid email address!";
+        myForm.reset();
     }
 });
-
