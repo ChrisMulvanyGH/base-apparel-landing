cb60b45a29041f09d89774cb3feb36c361fc382c
mitlIE <86919933+mitlIE@users.noreply.github.com>
2022-05-22 10:08:45 +0100

    JS best practices JS IIFE, Use Strict, Const

--- main.js
+++ main.js
@@ -1,27 +1,31 @@
-/* Get the required form elements */
-var myForm = document.querySelector('.subForm');
-var myField = document.querySelector('.subForm__email');
-var myBtn = document.querySelector('.subForm__button');
-var myMsg = document.querySelector('.msg');
-var errIcon = document.querySelector('.subForm__error-icon');
-var emailRegex = /^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/;
+(function () {
 
-// When the Submit button is clicked pass in the event
-myBtn.addEventListener('click', function(event){
-    event.preventDefault();
+    'use strict';
 
-    // Test to see if field is empty or has content
-    if(myField.value != "" && emailRegex.test(myField.value))  {
+    /* Get the required form elements */
+    const myForm = document.querySelector('.subForm');
+    const myField = document.querySelector('.subForm__email');
+    const myBtn = document.querySelector('.subForm__button');
+    const myMsg = document.querySelector('.msg');
+    const errIcon = document.querySelector('.subForm__error-icon');
+    const emailRegex = /^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/;
 
-        myMsg.classList.add('msg-show');
-        myMsg.innerHTML = "You have successfully subscribed!";
-        myForm.reset();
-    }
-    else
-    {
-        errIcon.classList.add('icon-show');
-        myMsg.classList.add('msg-show');
-        myMsg.innerHTML = "Please enter a valid email address!";
-        myField.focus();
-    }
-});
+    // When the Submit button is clicked pass in the event
+    myBtn.addEventListener('click', function (event) {
+        event.preventDefault();
+
+        // Test to see if field is empty or has content
+        if (myField.value != "" && emailRegex.test(myField.value)) {
+
+            myMsg.classList.add('msg-show');
+            myMsg.innerHTML = "You have successfully subscribed!";
+            myForm.reset();
+        } else {
+            errIcon.classList.add('icon-show');
+            myMsg.classList.add('msg-show');
+            myMsg.innerHTML = "Please enter a valid email address!";
+            myField.focus();
+        }
+    });
+
+})();
